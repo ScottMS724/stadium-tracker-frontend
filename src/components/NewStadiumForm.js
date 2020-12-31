1e8554ea45ee31119c90712d429fdc88a79b00e3
scottms724 <scottms724@gmail.com>
2020-12-31 13:06:36 -0500

    new stadium form & basic navbar

--- src/components/NewStadiumForm.js
+++ src/components/NewStadiumForm.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { updateNewStadiumForm } from '../actions/newStadiumForm.js'
+import { connect } from 'react-redux'
 
-const NewStadiumForm = () => (
-    <form>
+const NewStadiumForm = (props, history) => {
 
-    </form>
-)
+    const handleChange = event => {
+        const { name, value } = event.target
+            updateNewStadiumForm(name, value)
 
-export default NewStadiumForm
+    }
+
+    const handleSubmit = event => event.preventDefault()
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name="name" placeholder="Stadium name" onChange={handleChange} value={props.newStadiumFormData.name} />
+            <input name="city" placeholder="Stadium location (city)" onChange={handleChange} value={props.newStadiumFormData.city} />
+            <input name="image" placeholder="image URL" onChange={handleChange} value={props.newStadiumFormData.image} />
+            <input type="submit" value="Create Stadium" />
+        </form>
+)}
+
+const mapStateToProps = state => {
+    return {
+        newStadiumFormData: state.newStadiumForm 
+    }
+}
+
+export default connect(mapStateToProps, { updateNewStadiumForm })(NewStadiumForm)
 
--- src/store.js
+++ src/store.js
@@ -3,13 +3,15 @@ import currentUserReducer from './reducers/currentUser.js'
 import loginFormReducer from './reducers/loginForm.js'
 import stadiumsReducer from './reducers/myStadiums.js'
 import signUpReducer from './reducers/signUpForm.js'
+import newStadiumFormReducer from './reducers/newStadiumForm.js'
 import thunk from 'redux-thunk' 
 
 const reducer = combineReducers({ 
     currentUser: currentUserReducer,
     loginForm: loginFormReducer,
     myStadiums: stadiumsReducer,
-    signUpForm: signUpReducer
+    signUpForm: signUpReducer,
+    newStadiumForm: newStadiumFormReducer 
   })
   
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
