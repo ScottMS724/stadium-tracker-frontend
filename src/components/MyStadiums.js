fe65ee3c0ad861ce644f885323345b71b01712af
scottms724 <scottms724@gmail.com>
2021-01-08 11:58:05 -0500

    add stadium index links to show pages

--- src/components/MyStadiums.js
+++ src/components/MyStadiums.js
@@ -1,12 +1,14 @@
 import React from 'react' 
 import { connect } from 'react-redux'
 import StadiumCard from './StadiumCard.js'
+import { Link } from 'react-router-dom'
 
 const MyStadiums = (props) => {
-    const stadiumCards = props.stadiums.map(s => <StadiumCard stadium={s} key={s.id} />)
-    return (
-        stadiumCards.length > 0 ? stadiumCards : null 
-    )
+    const stadiumCards = props.stadiums.length > 0 ?
+    props.stadiums.map(s => (<><Link key={s.id} to={`/stadiums/${s.id}`}>{s.attributes.name}</Link><br/></>)) :
+    null
+    
+    return stadiumCards 
 }
 
 const mapStateToProps = state => { 
@@ -16,3 +18,4 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(MyStadiums)
+
