ff77c1ee47a113de66941ba20e0fbc3b024f989d
scottms724 <scottms724@gmail.com>
2021-02-06 11:36:53 -0500

    expected nested behavior for visitcard url and display

--- src/components/StadiumCard.js
+++ src/components/StadiumCard.js
@@ -1,13 +1,14 @@
 import React from 'react' 
-import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import MyVisits from './MyVisits.js'
 import VisitCard from './VisitCard.js'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const StadiumCard = ({stadium, visits, match}) => {
     return (
         stadium ?
+        <Router>
         <div>
             <h2>{stadium.attributes.name}</h2>
             <p>{stadium.attributes.city}</p>
@@ -20,9 +21,9 @@ const StadiumCard = ({stadium, visits, match}) => {
             <h3>Here are all your past visits to this stadium:</h3>
 
             <MyVisits visits={visits} matchId={match.params.id} matchURL={match.url} />
-
+            
             <Route path={`${match.url}/visits/:visitId`} render={routerProps => <VisitCard {...routerProps} visits={visits} /> }/>
-        </div> :
+        </div></Router> :
         null 
     )
 }
