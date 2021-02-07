205b62e73b88f4439735db3e349decfdb0fdedd2
scottms724 <scottms724@gmail.com>
2021-02-07 13:53:38 -0500

    visitform displays appropriately. submit functionality WIP

--- src/actions/myVisits.js
+++ src/actions/myVisits.js
@@ -1,8 +1,17 @@
+import { resetVisitForm } from './visitForm'
+
 // synchronous actions 
 export const setMyVisits = visits => {
     return {
         type: "SET_MY_VISITS",
         visits
+    }
+}
+
+export const addVisit = visit => {
+    return {
+        type: "ADD_VISIT",
+        visit
     }
 }
 
@@ -27,4 +36,34 @@ export const getMyVisits = () => {
     })
     .catch(console.log)
   }
+}
+
+export const createVisit = (visitData, history) => {
+    return dispatch => {
+        const sendableVisitData = {
+            date: visitData.date,
+            description: visitData.description,
+            stadium_id: visitData.stadium_id
+        }
+        return fetch("http://localhost:3001/api/v1/visits", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(sendableVisitData)
+        })
+            .then(resp => resp.json())
+            .then(visit => {
+                if (visit.error) {
+                    alert(visit.error)
+                } else {
+                    dispatch(addVisit(visit.data))
+                    dispatch(resetVisitForm())
+                    history.push(`/stadiums/${visit.data.stadium_id}`)
+                }
+            })
+            .catch(console.log)
+    }
+
 }
--- src/reducers/myVisits.js
+++ src/reducers/myVisits.js
@@ -7,8 +7,8 @@ const visitsReducer = (state = initialState, action) => {
     switch (action.type) {
         case "SET_MY_VISITS":
             return action.visits 
-        // case "ADD_VISITS":
-        //     return state.concat(action.visit)
+        case "ADD_VISITS":
+            return state.concat(action.visit)
         // case "UPDATE_VISITS":
         //     return state.map(visit => visit.id === action.visit.id ? action.visit : visit)
         // case "DELETE_VISITS":
