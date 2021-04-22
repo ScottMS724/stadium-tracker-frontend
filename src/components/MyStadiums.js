e57d9555134e3003c3bce08a96f8e523679b23fc
scottms724 <scottms724@gmail.com>
2021-04-22 14:32:39 -0400

    basic search functionality for stadiums

--- src/components/MyStadiums.js
+++ src/components/MyStadiums.js
@@ -3,9 +3,19 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const MyStadiums = (props) => {
+    const [searchTerm, setSearchTerm] = React.useState("");
+    // const [searchResults, setSearchResults] = React.useState(props.stadiums);
+    const searchResults = props.stadiums.filter(stadium => 
+        stadium.attributes.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     const stadiumCards = props.stadiums.length > 0 ?
-    props.stadiums.map(s => (<h1><p key={s.id}><Link key={s.id} to={`/stadiums/${s.id}`}>{s.attributes.name}</Link></p></h1>)) :
+    searchResults.map(s => (<h1><p key={s.id}><Link key={s.id} to={`/stadiums/${s.id}`}>{s.attributes.name}</Link></p></h1>)) :
     <h3 style={{color: "yellow"}}>You have no stadiums yet. Click the button above to add your first stadium.</h3>
+
+    const handleChange = e => {
+        setSearchTerm(e.target.value);
+    };
     
     return (
         <div className="bg2">
@@ -13,6 +23,7 @@ const MyStadiums = (props) => {
             <br></br>
             <h1 className="header_title">Your Stadiums</h1>
             {stadiumCards}
+            <input type="text" placeholder="search" value={searchTerm} onChange={handleChange}></input>
         </div>
     )
 }
